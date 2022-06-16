398e6171412c06f9dd0e824d1c72a7c2c8419897
吴宏彬 <66043405+TickHeart@users.noreply.github.com>
2022-06-16 11:15:11 +0800

    update title

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,39 +1,43 @@
-import { defineConfig } from "vitepress"
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
-  title: 'canvas-engine',
-  description: 'a framework to help you create your own canvas application',
+  title: "ztcanvas",
+  description: "a framework to help you create your own canvas application",
   lastUpdated: true,
   markdown: {
-    theme: 'material-palenight',
-    lineNumbers: true
+    theme: "material-palenight",
+    lineNumbers: true,
   },
   themeConfig: {
     logo: "/logo.png",
     footer: {
-      message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2022-present Tick Wu and Alex Zhang'
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2022-present Tick Wu and Alex Zhang",
     },
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/zxTick/canvas-engine' },
+      { icon: "github", link: "https://github.com/zxTick/canvas-engine" },
     ],
     editLink: {
-      pattern: 'https://github.com/zxTick/canvas-engine-docs/edit/main/docs/:path',
-      text: 'Edit this page on GitHub'
+      pattern:
+        "https://github.com/zxTick/canvas-engine-docs/edit/main/docs/:path",
+      text: "Edit this page on GitHub",
     },
     sidebar: {
       "/guide/": [
         {
           text: "Guide",
-          items: [{
-            text: "Getting Started",
-            link: "/guide/getting-started"
-          }, {
-            text: "Contribute Guide",
-            link: "/guide/contribute"
-          }]
-        }
-      ]
-    }
+          items: [
+            {
+              text: "Getting Started",
+              link: "/guide/getting-started",
+            },
+            {
+              text: "Contribute Guide",
+              link: "/guide/contribute",
+            },
+          ],
+        },
+      ],
+    },
   },
-})
+});

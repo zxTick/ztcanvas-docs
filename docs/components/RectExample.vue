1b2856dc77ed8ee4dc0d9f0157e39c9273da78b2
tickheart <66043405+TickHeart@users.noreply.github.com>
2022-06-19 19:18:38 +0800

    deps: 升级了 ztcanvas 的版本

--- docs/components/RectExample.vue
+++ docs/components/RectExample.vue
@@ -1,27 +1,25 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { CanvasEngine, Rect } from 'ztcanvas'
+import { onMounted } from "vue";
+import { CanvasEngine, Rect } from "ztcanvas";
 
 onMounted(() => {
   const engine = new CanvasEngine({
     w: "200",
-    h: "200"
-  })
+    h: "200",
+  });
   const rect = new Rect({
     x: 10,
     y: 10,
     w: 100,
     h: 100,
-    zIndex: 0
-  })
+    zIndex: 0,
+  });
   engine.render(rect, {
-    color: '#dd6d50'
-  })
-})
-
+    color: "#dd6d50",
+  });
+});
 </script>
 <template>
   <canvas id="canvas" style="background-color: #aaa"></canvas>
 </template>
-<style scoped>
-</style>
+<style scoped></style>
